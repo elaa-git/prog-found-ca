b5f73bdf73dd921507266c228a2178d3ff6efe2d
elaa-git <69598207+elaa-git@users.noreply.github.com>
2020-10-04 21:41:12 +0200

    Add files via upload

---
+++ level-1.js
@@ -0,0 +1,92 @@
+// question 1
+
+var carBrand = "bmw";
+
+// question 2
+
+var person = {
+    name: "Bill",
+    age: 32
+};
+
+// question 3
+
+var outOfStock = true;
+
+if(outOfStock === true) {
+    console.log("Out of stock");
+} 
+else {
+    console.log("In stock");
+}
+
+// question 4
+
+var numbersArray = [1, 2, 3, 4, 5];
+
+for(var i = 0; i < numbersArray.length; i++) {
+    console.log(numbersArray[i]);
+}
+
+// question 5
+
+for(var count = 15; count <= 25; count++) {
+    console.log(count);
+}
+
+// question 6
+
+for(var count = 15; count <= 25; count++) {
+    if (count === 20) 
+    console.log(count);
+}
+
+// question 7
+
+var cars = [
+    {
+      "brand": "bmw",
+      "hk": 170,
+      "electric": true
+    },
+    {
+      "brand": "VW",
+      "hk": 110,
+      "electric": false
+    }
+];
+  
+for(var i=0; i < cars.length; i++){
+    console.log(cars[i].hk);
+    console.log(cars[i].electric);
+}
+    
+
+// question 8
+
+function whatIDontLike(fruit) {
+    console.log("I don’t like " + fruit);
+}
+
+whatIDontLike("apple");
+
+
+// question 9
+
+function twoArg(arg1, arg2) {
+    var sum = arg1 - arg2;
+    console.log(sum);
+}
+
+twoArg(10, 2);
+
+// question 10
+
+var myArray = [];
+
+function myFunction(one) {
+    var myArray = one;
+    console.log(myArray);
+}
+
+myFunction(10);
